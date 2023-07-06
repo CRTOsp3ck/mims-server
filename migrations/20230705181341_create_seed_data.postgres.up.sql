8d6ca53292c4284a768ed9fe91d63543a9d72604
SP3CK <crtosp3ck@gmail.com>
2023-07-07 01:27:56 +0800

    changed seed data query

--- migrations/20230705181341_create_seed_data.postgres.up.sql
+++ migrations/20230705181341_create_seed_data.postgres.up.sql
@@ -8,8 +8,8 @@ INSERT INTO public.item (name,des,created_at,updated_at) VALUES
 	 ('MD2 Juice','Freshly pressed high quality pineapple juice, 100% natural with no sugar added','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000'),
 	 ('MD2 Sliced Fruit','Freshly cut fruit, you can taste its original sweetness','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000');
 INSERT INTO public.inventory (start_item_bal,end_item_bal,created_at,updated_at) VALUES
-	 ('0=150','0=5','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000'),
-	 ('0=520&1=50','0=14&1=15','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000');
+	 ('1=150','1=5','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000'),
+	 ('1=520&2=50','1=14&2=15','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000');
 INSERT INTO public.operation (start_time,end_time,location,agent_id,total_sales_qty,total_cost,total_sales_amount,net_profit,balance_id,inventory_id,created_at,updated_at) VALUES
 	 ('2023-01-07 00:00:00','2023-01-07 00:00:00','Test Location',1,150,330.89,1200,869.11,1,1,'2023-06-07 00:00:00','2023-06-07 00:00:00');
 INSERT INTO public.sale (amount,quantity,payment_type,operation_id,item_id,created_at,updated_at) VALUES
