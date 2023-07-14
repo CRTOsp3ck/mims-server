a2a28bf3229e9dafa2314de4fd86136965699c90
SP3CK <crtosp3ck@gmail.com>
2023-07-15 02:24:32 +0800

    update seed data

--- migrations/20230705181341_create_seed_data.postgres.up.sql
+++ migrations/20230705181341_create_seed_data.postgres.up.sql
@@ -5,8 +5,9 @@ INSERT INTO public.balance (bal_cash,bal_qr,created_at,updated_at) VALUES
 	 ('sb=500&eb=1270','sb=100&eb=561','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000'),
 	 ('sb=1159&eb=1791','sb=561&eb=241','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000');
 INSERT INTO public.item (name,des,created_at,updated_at) VALUES
-	 ('MD2 Juice','Freshly pressed high quality pineapple juice, 100% natural with no sugar added','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000'),
-	 ('MD2 Sliced Fruit','Freshly cut fruit, you can taste its original sweetness','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000');
+	 ('MD2 Raw Fruit','Raw fruit for you to create your own experience!','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000'),
+	 ('MD2 Sliced Fruit','Freshly cut fruit, you can taste its original sweetness','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000'),
+	 ('MD2 Juice','Freshly pressed high quality pineapple juice, 100% natural with no sugar added','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000');
 INSERT INTO public.inventory (start_item_bal,end_item_bal,created_at,updated_at) VALUES
 	 ('1=150','1=5','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000'),
 	 ('1=520&2=50','1=14&2=15','2023-06-07 00:00:00.000','2023-06-07 00:00:00.000');
